1e99edab6414e832a408687b303cacef3732a3e8
zjnzzz <1334029450@qq.com>
2017-10-21 15:14:12 +0800

    改showpanel.java,修改变量名，给部分变量加final by yuxiaohao

--- 1153710511-lab1-code/src/lab1/showpanel.java
+++ 1153710511-lab1-code/src/lab1/showpanel.java
@@ -18,8 +18,8 @@ public class showpanel extends JFrame{
   /** .
     * 
   */
-  public showpanel(graph p){
-    spanel s = new spanel(p);
+  public showpanel(graph padd){
+    final spanel s = new spanel(padd);
     this.add(s);
     this.setSize(1000,800);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,12 +29,12 @@ public class showpanel extends JFrame{
 
 class spanel extends JPanel{
   public spanel(graph p){
-    Button button0 = new Button("0退出！");
-    Button button1 = new Button("1展示有向图");
-    Button button2 = new Button("2查询桥接词");
-    Button button3 = new Button("3根据桥接词生成新文本");
-    Button button4 = new Button("4计算两个单词之间的最短路径");
-    Button button5 = new Button("5随机游走");
+    final Button button0 = new Button("0退出！");
+    final Button button1 = new Button("1展示有向图");
+    final Button button2 = new Button("2查询桥接词");
+    final Button button3 = new Button("3根据桥接词生成新文本");
+    final Button button4 = new Button("4计算两个单词之间的最短路径");
+    final Button button5 = new Button("5随机游走");
     this.add(button0);
     this.add(button1);
     this.add(button2);
@@ -50,9 +50,9 @@ class spanel extends JPanel{
       }
     });
     button1.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        dpic d = new dpic();
-        d.showDirectedGraph(p);
+      public void actionPerformed(final ActionEvent e) {
+        final dpic dadd = new dpic();
+        dadd.showDirectedGraph(p);
         //String choose1=JOptionPane.showInputDialog("是否关闭？");
         //  if(choose1.equals("是")){
         //  d.close();
@@ -60,14 +60,14 @@ class spanel extends JPanel{
       }
     });
     button2.addActionListener(new ActionListener() {
-      public void actionPerformed(ActionEvent e) {
-        String ss = JOptionPane.showInputDialog("请输入两个单词：\n");
-        String []stmp = ss.split(" ");
-        String bridge = p.queryBridgeWords(stmp[0],stmp[1]);
+      public void actionPerformed(final ActionEvent eadd) {
+        final String ssadd = JOptionPane.showInputDialog("请输入两个单词：\n");
+        final String []stmp = ssadd.split(" ");
+        final String bridge = p.queryBridgeWords(stmp[0],stmp[1]);
         if (bridge == null) {
           //System.out.println("没有桥接词！");
           JOptionPane.showMessageDialog(null, "没有桥接词！");
-        } else if (bridge == " ") {
+        } else if (bridge.equals(" ")) {
           JOptionPane.showMessageDialog(null,"没有word1或者word2");
           } else {
             //System.out.println(bridge);
@@ -77,21 +77,21 @@ class spanel extends JPanel{
     });
     button3.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        String inputtext = JOptionPane.showInputDialog("输入文本：\n");
-        String rst = p.generateNewText(inputtext);
+        final String inputtext = JOptionPane.showInputDialog("输入文本：\n");
+        final String rst = p.generateNewText(inputtext);
         JOptionPane.showMessageDialog(null,rst);
       }
     });
     button4.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        dpath d = new dpath();
-        d.showDirectedGraph(p);
+        final dpath dadd = new dpath();
+        dadd.showDirectedGraph(p);
       }
     });
     button5.addActionListener(new ActionListener() {
       public void actionPerformed(ActionEvent e) {
-        drandom d = new drandom();
-        d.showDirectedGraph(p); 
+        final drandom dadd = new drandom();
+        dadd.showDirectedGraph(p); 
       }
     });
   }
